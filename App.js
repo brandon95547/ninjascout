aab571b8beae7bfae8670ed4966c41ca5687893e
Admin <admin@Admins-Air.attlocal.net>
2021-11-14 15:34:31 -0500

    navigation, home screen

--- App.js
+++ App.js
@@ -1,75 +1,31 @@
-import React, { Component } from "react";
-import { TouchableOpacity } from "react-native";
-
-// navigation
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { UserProvider } from "./UserContext";
-// screens
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./components/screens/HomeScreen";
-import Login from "./components/screens/Login";
-import SignUp from "./components/screens/SignUp";
-import Scout from "./components/screens/Scout";
-import { componentStyles, colors } from "./src/GlobalStyles";
-import SideBar from "./components/SideBar";
 
-const global = require("./src/styles/global");
+const Stack = createNativeStackNavigator();
 
-const Stack = createStackNavigator();
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  drawerContent = (navigation) => {
-    return (
-      <TouchableOpacity style={global.animatedBox}>
-        <SideBar navigation={navigation} toggleOpen={this.toggleOpen} />
-      </TouchableOpacity>
-    );
-  };
-
-  render() {
-    return (
-      <UserProvider>
-        <NavigationContainer>
-          <Stack.Navigator headerMode="none">
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{ title: "Home" }}
-              initialParams={{
-                drawerContent: this.drawerContent,
-              }}
-            />
-            <Stack.Screen
-              name="Login"
-              component={Login}
-              options={{ title: "Login" }}
-              initialParams={{
-                drawerContent: this.drawerContent,
-              }}
-            />
-            <Stack.Screen
-              name="SignUp"
-              component={SignUp}
-              options={{ title: "Sign Up" }}
-              initialParams={{
-                drawerContent: this.drawerContent,
-              }}
-            />
-            <Stack.Screen
-              name="Scout"
-              component={Scout}
-              options={{ title: "Scout" }}
-              initialParams={{
-                drawerContent: this.drawerContent,
-              }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </UserProvider>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
